86f8ab21288ce9981a4864ff14b0deaacec51dbe
Ash530888 <67915974+Ash530888@users.noreply.github.com>
2022-03-23 12:17:07 +0000

    Update FiveDayForecast.js

--- src/pages/FiveDayForecast.js
+++ src/pages/FiveDayForecast.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Card from './Card';
-import Sidebar from '../components/Sidebar';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 class FiveDayForecast extends React.Component {
   //TODO: take location, unit etc. from settings
@@ -37,9 +35,6 @@ class FiveDayForecast extends React.Component {
   render() {
     return (
       <div className="container">
-        <Router>
-          <Sidebar />
-        </Router>
         <h1 className="display-1 jumbotron">5-Day Forecast</h1>
         <h5 className="display-5 text-muted">{this.state.city+", "+this.state.country}</h5>
         <div className="row justify-content-center" id='row justify-content-center'>
