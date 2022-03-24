54a21e671987c81d12a175ed86f1680a503760f0
Ash530888 <67915974+Ash530888@users.noreply.github.com>
2022-03-24 11:37:17 +0000

    Update SidebarData.js

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -31,7 +31,7 @@ export const SidebarData = [
       },
       {
         title: 'Hourly',
-        path: '',
+        path: '/Hourly',
         icon: <AiIcons.AiOutlineCloud />,
         className: 'nav-text',
       }
